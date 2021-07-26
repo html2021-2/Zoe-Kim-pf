e1f33d811565d271fdaa1d980b7e27f5ac6b70ca
html2021-2 <html2021-2@daum.net>
2021-07-26 15:04:20 +0900

    top 버튼

--- js/common.js
+++ js/common.js
@@ -1,32 +1,56 @@
 $(document).ready(function () {
-
-  // 스크롤 이벤트 선언
-  $(window).on('scroll', function () {
-    // scrollY = 스크롤바의 수직이동거리
-    const scrollY = $(this).scrollTop();
-    // console.log(scrollY);
-
-    // 스크롤바의 수직이동거리가 813픽셀 이상이면 .header.active
-    if (scrollY >= 813) $('.header').addClass('active');
-    else $('.header').removeClass('active');
-
-    // 스크롤바의 수직이동거리가 100픽셀 이상이면  #topBtn에 fadeIn
-    if (scrollY >= 100) $('#topBtn').stop().fadeIn();
-    else $('#topBtn').stop().fadeOut();
+  // 네비게이션 클릭시 이동
+  $('.pc_nav ul li a').on('click', function (e) {
+    e.preventDefault(); // a 태그의 기본 기능 제한!!!
+    $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 400, 'swing');
   });
 
-  // #topBtn 버튼을 클릭하면 스크롤바를 0으로 만들기
+  // #topBtn 버튼을 클릭시 상단으로 이동
   $('#topBtn').on('click', function () {
     $('html, body').stop().animate({scrollTop: 0}); // html -> ie, body -> 크롬
   });
+  
+  // 스크롤 이벤트
+  $(window).on('scroll', function () {
+    var scrollTop = $(this).scrollTop();
+    var $header = $("header");
+    //console.log(scrollTop, $('.flow').offset().top);
 
-  // PC 네비게이션 클릭시 상단으로 부드럽게 이동
-  $('.pc_nav ul li a').on('click', function () {
-    const $tg = $( $(this).attr('href') );
-    console.log($tg);
-    $('html, body').stop().animate({scrollTop: $tg.offset().top}, 'slow');
-    return false;
+    // #topBtn
+    if (scrollTop < 20) {
+      gsap.to('#topBtn', {display: 'none', opacity: 0, duration: 0.5});
+    } else {
+      if (scrollTop === $(document).height() - $(window).height()) gsap.to('#topBtn', {display: 'block', opacity: 1, bottom: 60, duration: 0.5})
+      else gsap.to('#topBtn', {display: 'block', opacity: 1, bottom: 30, duration: 0.5})
+    }
+
+    // fadeIn(), fadeOut()에 stop이 이상하다....
+    // if (scrollTop < 20) {
+    //   $('#topBtn').fadeOut();
+    // } else {
+    //   $('#topBtn').fadeIn();
+    //   if (scrollTop === $(document).height() - $(window).height()) $('#topBtn').stop().animate({bottom: 60}, 300);
+    //   else $('#topBtn').stop().animate({bottom: 30}, 300);
+    // }
+
+    // 헤더 상단 고정 & 풀기
+    if(scrollY < $(window).height()){
+      $header.removeClass("fixed");
+      $header.find(">div").attr("class","shadow1");
+      var $top = $(window).height()-$header.outerHeight();
+      var $header_p = $top+($header.outerHeight()*(scrollTop/$(window).height()));
+      $header.css({"top": $header_p});
+    }else{
+      $header.addClass("fixed");
+      $header.find(">div").attr("class","shadow2");
+    }
+
+
+
+
+
   });
-
+  
+  $(window).trigger('scroll');
 
 });
--- js/main.js
+++ js/main.js
@@ -1,29 +1,11 @@
 $(document).ready(function () {
+  // 메인페이지 높이 지정
+  $(window).trigger("scroll");
+  $("#main").css("height",$(window).height());
 
-  // #main
-  // #scrollBtn 버튼 클릭하면 스크롤바를 937(#about 위치)로 내림
+  // #scrollBtn 버튼 클릭하면 스크롤바를 #about 위치로 내림
   $('#scrollBtn').on('click', function () {
-    $('html, body').stop().animate({scrollTop: 937});
+    $('html, body').stop().animate({scrollTop: $('#about').offset().top}, 400, 'swing');
   });
   
-  // #about
-  // #age -> 만 나이 계산법
-  const now = new Date();
-  // console.log(now);
-
-  const yy = now.getFullYear();   // 연도 반환
-  const mm = now.getMonth() + 1;  // 월 반환 : 0 ~ 11월
-  const dd = now.getDate();       // 일 반환 : 1 ~ 31일
-  // console.log(yy, mm, dd);
-
-  // result 값
-  let result = yy - '1997';
-  // console.log(result);
-
-  // #age 찾아서 내부에 result 출력 (대상.innerHTML = 값;)
-  const Age = document.getElementById('age');
-
-  if(mm >= 11 && dd >= 27) Age.innerHTML = result;
-  else Age.innerHTML = result - '1';  // 아직 생일을 맞지 않았다면 -1살
-
 });
