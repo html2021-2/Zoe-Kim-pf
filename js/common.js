ee701644f690bec23bd747251fe4ad66eacab216
html2021-2 <html2021-2@daum.net>
2021-07-27 14:06:19 +0900

    .contact_btn 접근

--- js/common.js
+++ js/common.js
@@ -12,45 +12,29 @@ $(document).ready(function () {
   
   // 스크롤 이벤트
   $(window).on('scroll', function () {
-    var scrollTop = $(this).scrollTop();
-    var $header = $("header");
-    //console.log(scrollTop, $('.flow').offset().top);
-
+    
     // #topBtn
-    if (scrollTop < 20) {
+    if ($(this).scrollTop() < 20) {
       gsap.to('#topBtn', {display: 'none', opacity: 0, duration: 0.5});
     } else {
-      if (scrollTop === $(document).height() - $(window).height()) gsap.to('#topBtn', {display: 'block', opacity: 1, bottom: 60, duration: 0.5})
+      if ($(this).scrollTop() === $(document).height() - $(window).height()) gsap.to('#topBtn', {display: 'block', opacity: 1, bottom: 60, duration: 0.5})
       else gsap.to('#topBtn', {display: 'block', opacity: 1, bottom: 30, duration: 0.5})
     }
-
-    // fadeIn(), fadeOut()에 stop이 이상하다....
-    // if (scrollTop < 20) {
-    //   $('#topBtn').fadeOut();
-    // } else {
-    //   $('#topBtn').fadeIn();
-    //   if (scrollTop === $(document).height() - $(window).height()) $('#topBtn').stop().animate({bottom: 60}, 300);
-    //   else $('#topBtn').stop().animate({bottom: 30}, 300);
-    // }
-
+    
     // 헤더 상단 고정 & 풀기
+    var $header = $("header");
     if(scrollY < $(window).height()){
       $header.removeClass("fixed");
       $header.find(">div").attr("class","shadow1");
       var $top = $(window).height()-$header.outerHeight();
-      var $header_p = $top+($header.outerHeight()*(scrollTop/$(window).height()));
+      var $header_p = $top+($header.outerHeight()*($(this).scrollTop()/$(window).height()));
       $header.css({"top": $header_p});
-    }else{
+    }
+    else{
       $header.addClass("fixed");
       $header.find(">div").attr("class","shadow2");
     }
 
-
-
-
-
   });
   
-  $(window).trigger('scroll');
-
 });
---
+++ js/contact.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // .contact_btn에 마우스 진입 시 애니메이션
+  /* $('.contact_btn').hover(
+    function () { //mouseenter
+      $('.contact_btn span').stop().animate({top: 10, left: 10}, 'fast');
+      if (!$('.contact_bg img').is(':animated')){
+        console.log('1 animate...');
+        $('.contact_bg img').stop().animate({bottom: '+=130', opacity: 1}, 300);
+      }
+      $('.contact_wrap strong').stop().animate({opacity: 1}, 300);
+    }, 
+    function () { //mouseleave
+      $('.contact_btn span').stop().animate({top: 0, left: 0}, 'fast');
+      if (!$('.contact_bg img').is(':animated')){
+        console.log('2 animate...');
+        $('.contact_bg img').stop().animate({bottom: '-=130', opacity: 0}, 300);
+      }
+      $('.contact_wrap strong').stop().animate({opacity: 0}, 300);
+    }
+  ); */
+
+  $('.contact_btn').on({
+    mouseenter: function () {
+      $(this).parent().addClass('on');
+    },
+    mouseleave: function () {
+      $(this).parent().removeClass('on');
+    }
+  });
+
+});
